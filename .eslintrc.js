e413670ef118139b4855cdecda8ca2451db9e568
Roland Schmitt <rschmitt@samhammer.de>
2021-12-23 16:44:26 +0100

    add eslint ruleset for sag & include in eslint config add eslint-plugin-import

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
     "plugin:vue/strongly-recommended",
     "eslint:recommended",
     "@vue/typescript/recommended",
+    "./.eslintsag",
     "plugin:prettier/recommended"
   ],
   parserOptions: {

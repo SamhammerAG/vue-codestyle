031b549274773d7fb62768a2cc536b43af0ec54f
Roland Schmitt <rschmitt@samhammer.de>
2021-12-27 12:42:09 +0100

    adds stylelint 14 with vue-cli 5

---
+++ .stylelintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  root: true,
+  extends: [
+    "stylelint-config-recommended-scss",
+    "stylelint-config-recommended-vue/scss",
+    "stylelint-prettier/recommended"
+  ]
+};
